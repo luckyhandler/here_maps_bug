dbb68b4328690fe3646b2cd36dc8d4142e0229e8
Nino Handler <ninohandler@gmail.com>
2020-05-28 23:00:12 +0200

    Styling

--- lib/main.dart
+++ lib/main.dart
@@ -53,6 +53,10 @@ class _MyHomePageState extends State {
 }
 
 class MapWidget extends StatefulWidget {
+  MapWidget({this.scheme = MapScheme.greyDay});
+
+
+  final MapScheme scheme;
   @override
   State<MapWidget> createState() {
     return MapState();
@@ -101,7 +105,7 @@ class MapState extends State<MapWidget> {
       BuildContext context, HereMapController mapController) async {
     mapController.gestures;
     _mapController = mapController;
-    mapController.mapScene.loadSceneForMapScheme(MapScheme.greyDay,
+    mapController.mapScene.loadSceneForMapScheme(widget.scheme,
         (MapError error) {
       if (error != null) {
         debugPrint(error.toString());
@@ -147,26 +151,35 @@ class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     List<int> list = List<int>.generate(10, (i) => i + 1);
-    return NestedScrollView(
-      headerSliverBuilder: (context, innerBoxIsScrolled) {
-        return [
-          SliverAppBar(
-              expandedHeight: 250.0,
-              floating: true,
-              pinned: true,
-              snap: false,
-              flexibleSpace: FlexibleSpaceBar(
-                title: Text('Second Screen'),
-                background: MapWidget(),
-              )),
-        ];
-      },
-      body: ListView.builder(
-        itemCount: list.length,
-        itemBuilder: (context, index) {
-          int item = list[index];
-          return Text('item $item');
+    return Scaffold(
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) {
+          return [
+            SliverAppBar(
+                expandedHeight: 250.0,
+                floating: true,
+                pinned: true,
+                snap: false,
+                flexibleSpace: FlexibleSpaceBar(
+                  title: Text('Second Screen'),
+                  background: MapWidget(scheme: MapScheme.greyNight),
+                )),
+          ];
         },
+        body: ListView.builder(
+          itemCount: list.length,
+          itemBuilder: (context, index) {
+            int item = list[index];
+            return Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ListTile(
+                leading: Icon(Icons.map),
+                title: Text('item $item'),
+                subtitle: Text('$item from ${list.length} items'),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
